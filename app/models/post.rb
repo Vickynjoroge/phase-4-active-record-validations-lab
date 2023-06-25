4c89f4fe450b9f435312fdcb5d0bd4faea1d46e6
Victor Ngigi Njoroge <vnjorogen2001@gmail.com>
2023-06-25 12:24:47 +0300

    Completed Assinment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    CATEGORY = ['Fiction', 'Non-fiction']
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: CATEGORY}
+    validate :clickbait_title, on: :title, on: :category, {in: CATEGORY}
+private
+
+  def clickbait_title
+    clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+    unless clickbait_phrases.any? { |phrase| title.include?(phrase) }
+      errors.add(:title, "should be clickbait-y")
+    end
+  end
 end
